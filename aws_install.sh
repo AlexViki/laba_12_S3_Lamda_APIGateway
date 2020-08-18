684d6dada1bf629da5dc388bedcd654bcca53835
alexviki <shevashokc@gmail.com>
2020-08-18 08:08:32 +0000

    Create S3 via Ansible shell module using AWS CLI

--- aws_install.sh
+++ aws_install.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update -y
 sudo apt-get install zip unzip -qy
-#curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 wget -O awscli-bundle.zip https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
 sudo unzip -f awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
